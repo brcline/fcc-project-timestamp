4ab43c330e544bbfc3df3787b4a69cc2098dee3c
Brian Cline <brian@caddle.ca>
2020-07-01 23:57:11 -0400

    timestamp endpoints challenge

--- server.js
+++ server.js
@@ -2,31 +2,47 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require('cors')
+app.use(cors({optionSuccessStatus: 200})) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({greeting: 'hello API'})
+})
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get('/api/timestamp/', function (req, res) {
+  res.json(getDate(new Date().toISOString()))
+})
 
+app.get('/api/timestamp/:date_string', function (req, res) {
+  res.json(getDate(req.params.date_string))
+})
 
+function getDate (d) {
+  const dateString = isFinite(d) ? parseInt(d) : d
+  const theDate = new Date(dateString)
+
+  if (!isNaN(theDate.getTime())) {
+    return {'unix': theDate.getTime(), 'utc': theDate.toUTCString()}
+  } else {
+    return {'error': 'Invalid Date'}
+  }
+}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+  console.log('Your app is listening on port ' + listener.address().port)
+})
